8c292daa8729e2f908c25ae4f42756f7629444d0
flores-97 <floresvictor15@gmail.com>
2025-10-29 21:30:22 -0700

    completed task in order.

--- LectureActivityFunctions.js
+++ LectureActivityFunctions.js
@@ -3,3 +3,44 @@ let myName = "Victor";
 let greetingFunc = "Hello, " + myName; {
     console.log("Hello, " + myName);
 }
+
+
+//*********************************** 
+
+const pi = 3.14;
+
+function circumference(pi , radius){
+    return pi * radius;
+}
+
+console.log(pi * 10);
+
+//*********************************** 
+
+function helloFrom() {
+    console.log("Hello from....");
+}
+
+function theOtherSide() {
+    helloFrom();
+    console.log("the other side!")
+    
+}
+
+helloFrom();
+theOtherSide();
+
+//*********************************** 
+// 4. I changed the order that so that my output is "Hello from ...." first so that it makes sense. Initially it would sandwich "the other side!" because it would execute the second function then call the first function
+
+//*********************************** 
+
+const startFire = () => {
+    return extFire();
+};
+
+const extFire = () => {
+    return startFire();
+};
+
+console.log(startFire( ));
